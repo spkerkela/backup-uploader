bf9f003b988334b7ec8b938d5802cfa65e67bfc3
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2023-01-11 19:59:29 +0200

    add build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash -euo pipefail
+
+GOOS=windows GOARCH=amd64 go build -o build/windows/amd64/backup-uploader.exe
+GOOS=darwin GOARCH=amd64 go build -o build/darwin/amd64/backup-uploader
+GOOS=darwin GOARCH=arm64 go build -o build/darwin/arm64/backup-uploader
+GOOS=linux GOARCH=amd64 go build -o build/linux/amd64/backup-uploader
+GOOS=linux GOARCH=arm go build -o build/linux/arm/backup-uploader
+GOOS=linux GOARCH=386 go build -o build/linux/386/backup-uploader
