e3421fae1cbe3ead2833c90074de1bc30a09a8fe
Simo-Pekka Kerkelä <simopekka.kerkela@spkerkela.com>
2023-01-13 18:26:44 +0200

    refactor build and create-release-packages.sh

--- create-release-packages.sh
+++ create-release-packages.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-tar -czf build/linux/amd64/backup-uploader.tar.gz -C build/linux/amd64/ backup-uploader
-tar -czf build/linux/arm/backup-uploader.tar.gz -C build/linux/arm/ backup-uploader
-tar -czf build/linux/386/backup-uploader.tar.gz -C build/linux/386/ backup-uploader
-tar -czf build/darwin/amd64/backup-uploader.tar.gz -C build/darwin/amd64/ backup-uploader
-tar -czf build/darwin/arm64/backup-uploader.tar.gz -C build/darwin/arm64/ backup-uploader
-tar -czf build/windows/amd64/backup-uploader.tar.gz -C build/windows/amd64/ backup-uploader.exe
+PROGRAM_NAME=$(basename "$PWD")
+
+tar -czf build/linux/amd64/"$PROGRAM_NAME".tar.gz -C build/linux/amd64/ "$PROGRAM_NAME"
+tar -czf build/linux/arm/"$PROGRAM_NAME".tar.gz -C build/linux/arm/ "$PROGRAM_NAME"
+tar -czf build/linux/386/"$PROGRAM_NAME".tar.gz -C build/linux/386/ "$PROGRAM_NAME"
+tar -czf build/darwin/amd64/"$PROGRAM_NAME".tar.gz -C build/darwin/amd64/ "$PROGRAM_NAME"
+tar -czf build/darwin/arm64/"$PROGRAM_NAME".tar.gz -C build/darwin/arm64/ "$PROGRAM_NAME"
+tar -czf build/windows/amd64/"$PROGRAM_NAME".tar.gz -C build/windows/amd64/ "$PROGRAM_NAME".exe
